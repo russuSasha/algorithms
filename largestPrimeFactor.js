4c272a7edf8c0c7288eff31e22fd3e6aa910c3c8
Sasha Russu <russusasha19@gmail.com>
2020-07-21 19:21:24 +0300

    fixed largestPrimeFactor

--- largestPrimeFactor.js
+++ largestPrimeFactor.js
@@ -1,11 +1,11 @@
-function getLargestPrimeFactor(num, factors = []) {
+function getLargestPrimeFactor(num) {
   if (num % 2 === 0) {
-    return getLargestPrimeFactor(num / 2, [...factors, 2])
+    return getLargestPrimeFactor(num / 2)
   }
 
   for (let i = 3; i < num / 3; i += 2) {
     if (num % i === 0) {
-      return getLargestPrimeFactor(num / i, [...factors, i])
+      return getLargestPrimeFactor(num / i)
     }
   }
 
